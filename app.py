31e258ead44c5310115773bb091391a04b82004e
Fatma-Chaouech <fatmachaouech@insat.u-carthage.tn>
2023-08-01 19:11:35 +0100

    added tika error handling

--- app.py
+++ app.py
@@ -80,70 +80,70 @@ def run(filename, content, voice_name, description, files):
     create_directory_if_not_exists(temp_dir)
     create_directory_if_not_exists(clone_dir)
 
-    # # generate sound effects embeddings
-    # if not vector_db.has_embeddings():
-    #     embedded_effects, dimension = get_sound_effects_embeddings("./sounds")
-    #     if not vector_db.has_index():
-    #         index = vector_db.create_pinecone_index(index_name, dimension=dimension)
-    #     vector_db.embeddings_to_pinecone(embedded_effects, index)
+    # generate sound effects embeddings
+    if not vector_db.has_embeddings():
+        embedded_effects, dimension = get_sound_effects_embeddings("./sounds")
+        if not vector_db.has_index():
+            index = vector_db.create_pinecone_index(index_name, dimension=dimension)
+        vector_db.embeddings_to_pinecone(embedded_effects, index)
 
-    # # split the book into paragraphs
-    # split_book = input_to_chunks(content)
+    # split the book into paragraphs
+    split_book = input_to_chunks(content)
 
-    # # choose voice based on random excerpt, if cloning is not selected
-    # excerpt_book = split_book[random.randint(0, len(split_book) - 1)]
-    # if not files:
-    #     voice_types = get_voices_info()
-    #     template = VoiceCategoryPrompt()
-    #     voice = query_model(template(voice_types, excerpt_book))
-    #     print("GPT has chosen {} for the voice actor...".format(voice))
+    # choose voice based on random excerpt, if cloning is not selected
+    excerpt_book = split_book[random.randint(0, len(split_book) - 1)]
+    if not files:
+        voice_types = get_voices_info()
+        template = VoiceCategoryPrompt()
+        voice = query_model(template(voice_types, excerpt_book))
+        print("GPT has chosen {} for the voice actor...".format(voice))
 
-    # # get the voice, if cloning is selected
-    # else:
-    #     filenames = []
-    #     for idx, file_ in enumerate(files):
-    #         filenames.append(clone_dir + "/{}_{}".format(voice_name, idx))
-    #         with open(filenames[idx], "wb") as f:
-    #             f.write(file_.getbuffer())
-    #     voice = clone(name=voice_name, description=description, files=filenames)
+    # get the voice, if cloning is selected
+    else:
+        filenames = []
+        for idx, file_ in enumerate(files):
+            filenames.append(clone_dir + "/{}_{}".format(voice_name, idx))
+            with open(filenames[idx], "wb") as f:
+                f.write(file_.getbuffer())
+        voice = clone(name=voice_name, description=description, files=filenames)
 
-    # # prepare the sound effects template
-    # template = SoundEffectsPrompt()
+    # prepare the sound effects template
+    template = SoundEffectsPrompt()
 
-    # # for each paragraph
-    # for idx1, split in enumerate(split_book):
-    #     # get the sound effects
-    #     split_with_sfx = query_model(template(split))
-    #     sound_effects = extract_sound_effects_from_text(split_with_sfx)
-    #     print("Extracted sound effects: ", sound_effects)
+    # for each paragraph
+    for idx1, split in enumerate(split_book):
+        # get the sound effects
+        split_with_sfx = query_model(template(split))
+        sound_effects = extract_sound_effects_from_text(split_with_sfx)
+        print("Extracted sound effects: ", sound_effects)
 
-    #     # split the paragraph by the sound effect, and remove them
-    #     refactored_split = chunk_and_remove_sfx(split_with_sfx)
+        # split the paragraph by the sound effect, and remove them
+        refactored_split = chunk_and_remove_sfx(split_with_sfx)
         
-    #     # for each subparagraph
-    #     for idx2, subparagraph in enumerate(refactored_split):
-    #         # send the audio to elevenlabs
-    #         audio = generate(subparagraph, voice=voice)
+        # for each subparagraph
+        for idx2, subparagraph in enumerate(refactored_split):
+            # send the audio to elevenlabs
+            audio = generate(subparagraph, voice=voice)
 
-    #         # store it
-    #         save(audio=audio, filename=temp_dir + f"/voice{idx1}_{idx2}.mp3")
+            # store it
+            save(audio=audio, filename=temp_dir + f"/voice{idx1}_{idx2}.mp3")
 
-    #         # get the corresponding sound effect, if there still is one
-    #         print(f"{idx1}.{idx2}")
-    #         if idx2 < len(sound_effects):
-    #             similar_effect = find_most_similar_effect(sound_effects[idx2], index)
+            # get the corresponding sound effect, if there still is one
+            print(f"{idx1}.{idx2}")
+            if idx2 < len(sound_effects):
+                similar_effect = find_most_similar_effect(sound_effects[idx2], index)
 
-    #             # store that sound effect
-    #             copy_file_with_new_name(
-    #                 "./sounds",
-    #                 similar_effect + ".mp3",
-    #                 temp_dir,
-    #                 str(f"sfx{idx1}_{idx2}.mp3"),
-    #             )
-    #             # sleep to avoid rate limit
-    #             time.sleep(20)
+                # store that sound effect
+                copy_file_with_new_name(
+                    "./sounds",
+                    similar_effect + ".mp3",
+                    temp_dir,
+                    str(f"sfx{idx1}_{idx2}.mp3"),
+                )
+                # sleep to avoid rate limit
+                time.sleep(20)
 
-    # audiobook = contruct_audiobook(temp_dir)
+    audiobook = contruct_audiobook(temp_dir)
 
     print("Audio generation...")
     # play(audiobook)
